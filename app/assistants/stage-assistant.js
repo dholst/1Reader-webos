b67332348d5a4fc5923b673f171a2fc42285887f
Darrin Holst <darrinholst@gmail.com>
2010-04-01 08:43:45 -0500

    change login item layout and start of copy functionality

--- app/assistants/stage-assistant.js
+++ app/assistants/stage-assistant.js
@@ -1,5 +1,12 @@
 StageAssistant = Class.create({
   setup: function() {
+    Mojo.Event.listen(document, Mojo.Event.tap, this.copy.bind(this));
     this.controller.pushScene("pick");
+  },
+
+  copy: function(event) {
+    if(event.target && event.target.hasClassName("copyable")) {
+      console.log("COPYING " + event.target.innerHTML + " TO CLIPBOARD")
+    }
   }
 });

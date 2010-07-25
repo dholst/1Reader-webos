8dba2bfc24e75fb816aa237dcea90ca3ea00c13d
Darrin Holst <darrinholst@gmail.com>
2010-07-25 12:51:51 -0500

    item assistant extends base assistant to get the app menu

--- app/assistants/item-assistant.js
+++ app/assistants/item-assistant.js
@@ -1,10 +1,11 @@
-ItemAssistant = Class.create({
+ItemAssistant = Class.create(BaseAssistant, {
   initialize: function(keychain, item) {
     this.keychain = keychain
     this.item = item
   },
   
-  setup: function() {
+  setup: function($super) {
+    $super()
     this.keychain.loadItem(this.item, this.itemLoaded.bind(this))
   }
 })
